b36a4b0a487ca82be3579d7a8167abbe702a1e71
Hitesh Rawat <hiteshrawat.92@gmail.com>
2021-10-25 23:22:23 +0530

    initial setup.

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Test')
+    .setDescription('The test description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(5000, '0.0.0.0');
+}
+bootstrap();
